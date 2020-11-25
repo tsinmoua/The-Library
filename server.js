1ca77d89b0c592584854670f19e849f2d72d9175
Tsin Moua <tsinmoua@gmail.com>
2020-11-24 23:33:42 -0600

    Added folders, created files for models folder

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port http://localhost:${PORT}`);
 });
