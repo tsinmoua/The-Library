19fb4ef288ef8da341917d93c04be182ba3d63a1
Tsin Moua <tsinmoua@gmail.com>
2020-11-25 19:06:18 -0600

    Added api route and updated server info with mongoose

---
+++ client/src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const APIKEY = process.env.REACT_APP_GOOGLE_API_KEY
+
+export default {
+  search: function(query) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+  }
+};
